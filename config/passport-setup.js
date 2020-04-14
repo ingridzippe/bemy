955adc52c675ffd3af09c04b609af07411ca1147
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-04-14 12:19:48 -0400

    google user

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -13,27 +13,27 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
         console.log("passport callback function fired")
-        console.log(profile);
-        new models.User({
-            username: profile.displayName,
-            googleId: profile.id
-        }).save().then((newUser) => {
-            console.log('new user created' + newUser);
-        })
-        // var u = new User({
+        // console.log(profile);
+        // new models.User({
         //     username: profile.displayName,
         //     googleId: profile.id
-        // });
-        // u.save(function(err, user) {
-        //     if (err) {
-        //         console.log(err);
-        //         res.status(500).redirect('/index');
-        //         return;
-        //     }
-        //     console.log("new user created");
-        //     console.log(user);
-        //     res.redirect('/index');
-        // });
+        // }).save().then((newUser) => {
+        //     console.log('new user created' + newUser);
+        // })
+        var u = new models.User({
+            username: profile.displayName,
+            googleId: profile.id
+        });
+        u.save(function(err, user) {
+            if (err) {
+                console.log(err);
+                res.status(500).redirect('/index');
+                return;
+            }
+            console.log("new user created");
+            console.log(user);
+            res.redirect('/index');
+        });
     })
 )
 
