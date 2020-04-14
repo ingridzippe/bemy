f52f3db78df47b9e2b4eb725fa99fb17c62bb2db
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-04-14 15:42:37 -0400

    linkedin

--- app.js
+++ app.js
@@ -150,170 +150,170 @@ app.get('/auth/google/redirect', passport.authenticate('google'), (req, res) => 
 
 // LINKEDIN STRATEGY
 var LinkedInStrategy = require('passport-linkedin').Strategy;
-// passport.use(new LinkedInStrategy({
-//     consumerKey: keys.linkedin.apiKey,
-//     consumerSecret: keys.linkedin.secretKey,
-//     callbackURL: "https://secret-fjord-13510.herokuapp.com/auth/linkedin/callback",
-//     profileFields: ['id', 'first-name', 'last-name', 'email-address', 'headline']
-//   },
-//   function(token, tokenSecret, profile, done) {
-// 	console.log("linkedin profile");
-// 	console.log(profile);
-//     // User.findOrCreate({ linkedinId: profile.id }, function (err, user) {
-//     //   return done(err, user);
-//     // });
-//   }
-// ));
-// app.get('/auth/linkedin',
-// 	passport.authenticate('linkedin', { scope: ['r_basicprofile', 'r_emailaddress'] }));
-// app.get('/auth/linkedin/callback', 
-//   	passport.authenticate('linkedin', { failureRedirect: '/login' }),
-//   	function(req, res) {
-//     	// Successful authentication, redirect home.
-//     	res.redirect('/');
+passport.use(new LinkedInStrategy({
+    consumerKey: keys.linkedin.apiKey,
+    consumerSecret: keys.linkedin.secretKey,
+    callbackURL: "https://secret-fjord-13510.herokuapp.com/auth/linkedin/callback",
+    profileFields: ['id', 'first-name', 'last-name', 'email-address', 'headline']
+  },
+  function(token, tokenSecret, profile, done) {
+	console.log("linkedin profile");
+	console.log(profile);
+    // User.findOrCreate({ linkedinId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+  }
+));
+app.get('/auth/linkedin',
+	passport.authenticate('linkedin', { scope: ['r_basicprofile', 'r_emailaddress'] }));
+app.get('/auth/linkedin/callback', 
+  	passport.authenticate('linkedin', { failureRedirect: '/login' }),
+  	function(req, res) {
+    	// Successful authentication, redirect home.
+    	res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
+// app.get('/linkedin', function(req, res) {
+// 	console.log("linkedin");
+// 	res.redirect("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78w1f2pk5r3x2y&redirect_uri=https://secret-fjord-13510.herokuapp.com/auth/linkedin/callback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social");
 // });
 
+// app.get('/auth/linkedin/callback', function(req, res) {
+// 	console.log("callback");
 
+// 	const accessCode = req.query.code;
+// 	var clientId = "78w1f2pk5r3x2y";
+// 	var clientSecret = "7XrmOoEUZC27RAUN";
+// 	var accessToken = null;
 
+// 	const Http = new XMLHttpRequest();
+// 	const url='https://www.linkedin.com/oauth/v2/accessToken?client_id='+clientId+'&client_secret='+clientSecret+'&grant_type=authorization_code&redirect_uri=https://secret-fjord-13510.herokuapp.com/auth/linkedin/callback&code='+accessCode;
+// 	Http.open("GET", url);
+// 	Http.send();
+// 	Http.onreadystatechange = (e) => {
+// 		console.log("type of HttpResonseText");
+// 		console.log(typeof Http.responseText);
+// 		accessToken = Http.responseText.slice(17, Http.responseText.length-23);
+// 		console.log(accessToken);
 
+// 		// This sample code will make a request to LinkedIn's API to retrieve and print out some
+// 		// basic profile information for the user whose access token you provide.
+// 		// Replace with access token for the r_liteprofile permission
+// 		if (accessToken != null) {
 
+// 		// printing info
+// 		const options = {
+// 			host: 'api.linkedin.com',
+// 			path: '/v2/me',
+// 			method: 'GET',
+// 			headers: {
+// 			'Authorization': `Bearer ${accessToken}`,
+// 			'cache-control': 'no-cache',
+// 			'X-Restli-Protocol-Version': '2.0.0'
+// 			}
+// 		};
+// 		const profileRequest = https.request(options, function(res) {
+// 			let data = '';
+// 			res.on('data', (chunk) => {
+// 			data += chunk;
+// 			});
+// 			res.on('end', () => {
+// 			console.log('gets in here?')
+// 			const profileData = JSON.parse(data);
+// 			var lastName = profileData.localizedLastName;
+// 			var firstName = profileData.localizedFirstName;
+// 			});
+// 		});
+// 		profileRequest.end();
 
+// 		// print image photo
+// 		// GET https://api.linkedin.com/v2/me?projection=(id,profilePicture(displayImage~digitalmediaAsset:playableStreams))
+// 		const options3 = {
+// 		  host: 'api.linkedin.com',
+// 		  path: '/v2/me?projection=(id,profilePicture(displayImage~digitalmediaAsset:playableStreams))',
+// 		  method: 'GET',
+// 		  headers: {
+// 		    'Authorization': `Bearer ${accessToken}`,
+// 		    'cache-control': 'no-cache',
+// 		    'X-Restli-Protocol-Version': '2.0.0'
+// 		  }
+// 		};
+// 		const profileRequest3 = https.request(options3, function(res) {
+// 		  let data = '';
+// 		  res.on('data', (chunk) => {
+// 		    data += chunk;
+// 		  });
+// 		  res.on('end', () => {
+// 		    console.log('prints email?')
+// 		    const profileData = JSON.parse(data);
 
+// 		    console.log("profileData");
+// 		    console.log(profileData);
 
-
-app.get('/linkedin', function(req, res) {
-	console.log("linkedin");
-	res.redirect("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78w1f2pk5r3x2y&redirect_uri=https://secret-fjord-13510.herokuapp.com/auth/linkedin/callback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social");
-});
-
-app.get('/auth/linkedin/callback', function(req, res) {
-	console.log("callback");
-
-	const accessCode = req.query.code;
-	var clientId = "78w1f2pk5r3x2y";
-	var clientSecret = "7XrmOoEUZC27RAUN";
-	var accessToken = null;
-
-	const Http = new XMLHttpRequest();
-	const url='https://www.linkedin.com/oauth/v2/accessToken?client_id='+clientId+'&client_secret='+clientSecret+'&grant_type=authorization_code&redirect_uri=https://secret-fjord-13510.herokuapp.com/auth/linkedin/callback&code='+accessCode;
-	Http.open("GET", url);
-	Http.send();
-	Http.onreadystatechange = (e) => {
-		console.log("type of HttpResonseText");
-		console.log(typeof Http.responseText);
-		accessToken = Http.responseText.slice(17, Http.responseText.length-23);
-		console.log(accessToken);
-
-		// This sample code will make a request to LinkedIn's API to retrieve and print out some
-		// basic profile information for the user whose access token you provide.
-		// Replace with access token for the r_liteprofile permission
-		if (accessToken != null) {
-
-		// printing info
-		const options = {
-			host: 'api.linkedin.com',
-			path: '/v2/me',
-			method: 'GET',
-			headers: {
-			'Authorization': `Bearer ${accessToken}`,
-			'cache-control': 'no-cache',
-			'X-Restli-Protocol-Version': '2.0.0'
-			}
-		};
-		const profileRequest = https.request(options, function(res) {
-			let data = '';
-			res.on('data', (chunk) => {
-			data += chunk;
-			});
-			res.on('end', () => {
-			console.log('gets in here?')
-			const profileData = JSON.parse(data);
-			var lastName = profileData.localizedLastName;
-			var firstName = profileData.localizedFirstName;
-			});
-		});
-		profileRequest.end();
-
-		// print image photo
-		// GET https://api.linkedin.com/v2/me?projection=(id,profilePicture(displayImage~digitalmediaAsset:playableStreams))
-		const options3 = {
-		  host: 'api.linkedin.com',
-		  path: '/v2/me?projection=(id,profilePicture(displayImage~digitalmediaAsset:playableStreams))',
-		  method: 'GET',
-		  headers: {
-		    'Authorization': `Bearer ${accessToken}`,
-		    'cache-control': 'no-cache',
-		    'X-Restli-Protocol-Version': '2.0.0'
-		  }
-		};
-		const profileRequest3 = https.request(options3, function(res) {
-		  let data = '';
-		  res.on('data', (chunk) => {
-		    data += chunk;
-		  });
-		  res.on('end', () => {
-		    console.log('prints email?')
-		    const profileData = JSON.parse(data);
-
-		    console.log("profileData");
-		    console.log(profileData);
-
-		    // console.log("JSON.stringify(profileData, 0, 2)");
-		    // console.log(JSON.stringify(profileData, 0, 2));
+// 		    // console.log("JSON.stringify(profileData, 0, 2)");
+// 		    // console.log(JSON.stringify(profileData, 0, 2));
 			
-		    // console.log("profileData.elements");
-		    // console.log(profileData.elements);
+// 		    // console.log("profileData.elements");
+// 		    // console.log(profileData.elements);
 
-		    // var profileString = JSON.stringify(profileData.elements);
-		    // console.log(profileString);
-			// var profileArray = profileString.split(`"`);
-			// console.log(profileArray);
-		  });
-		});
-		profileRequest3.end();
+// 		    // var profileString = JSON.stringify(profileData.elements);
+// 		    // console.log(profileString);
+// 			// var profileArray = profileString.split(`"`);
+// 			// console.log(profileArray);
+// 		  });
+// 		});
+// 		profileRequest3.end();
 
 
-		// printing email
-		const options2 = {
-			host: 'api.linkedin.com',
-			path: '/v2/emailAddress?q=members&projection=(elements*(handle~))',
-			method: 'GET',
-			headers: {
-			'Authorization': `Bearer ${accessToken}`,
-			'cache-control': 'no-cache',
-			'X-Restli-Protocol-Version': '2.0.0'
-			}
-		};
-		const profileRequest2 = https.request(options2, function(res) {
-			let data = '';
-			res.on('data', (chunk) => {
-			data += chunk;
-			});
-			res.on('end', () => {
-			console.log('prints email')
-			const profileData = JSON.parse(data);
-			// console.log(profileData);
+// 		// printing email
+// 		const options2 = {
+// 			host: 'api.linkedin.com',
+// 			path: '/v2/emailAddress?q=members&projection=(elements*(handle~))',
+// 			method: 'GET',
+// 			headers: {
+// 			'Authorization': `Bearer ${accessToken}`,
+// 			'cache-control': 'no-cache',
+// 			'X-Restli-Protocol-Version': '2.0.0'
+// 			}
+// 		};
+// 		const profileRequest2 = https.request(options2, function(res) {
+// 			let data = '';
+// 			res.on('data', (chunk) => {
+// 			data += chunk;
+// 			});
+// 			res.on('end', () => {
+// 			console.log('prints email')
+// 			const profileData = JSON.parse(data);
+// 			// console.log(profileData);
 		
-			// console.log("JSON.stringify(profileData, 0, 2)");
-			// console.log(JSON.stringify(profileData, 0, 2));
+// 			// console.log("JSON.stringify(profileData, 0, 2)");
+// 			// console.log(JSON.stringify(profileData, 0, 2));
 		
-			console.log("profileData.elements");
-			console.log(profileData.elements);
+// 			console.log("profileData.elements");
+// 			console.log(profileData.elements);
 		
-			// var profileString = JSON.stringify(profileData.elements);
-			// console.log(profileString);
-			// var profileArray = profileString.split(`"`);
-			// console.log(profileArray);
-			});
-		});
-		profileRequest2.end();
+// 			// var profileString = JSON.stringify(profileData.elements);
+// 			// console.log(profileString);
+// 			// var profileArray = profileString.split(`"`);
+// 			// console.log(profileArray);
+// 			});
+// 		});
+// 		profileRequest2.end();
 
-		}
+// 		}
 
-	}
+// 	}
 
-	res.render('login')
-});
+// 	res.render('login')
+// });
 
 
 
