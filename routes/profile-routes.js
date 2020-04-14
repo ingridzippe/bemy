2229922b7ed781f04ef414d1025d70239822c836
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-04-14 13:58:54 -0400

    add profile page

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send("you are logged in - " + req.user.username);
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        // if user is not logged in
+        res.redirect('/auth/login');
+    } else {
+        // if logged in
+        next();
+    }
+};
+
+router.get('/', authCheck, (req, res) => {
+    res.render('profile');
 })
 
 module.exports = router;
