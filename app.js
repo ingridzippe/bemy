f796328d421b8600bac906852534732b9e0cd2cc
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-04-14 18:20:42 -0400

    linkedin email

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ passport.use(new LinkedInStrategy({
 	console.log(profile.id);
 	console.log(typeof profile.id);
 	console.log("PROFILE JSON EMAILS")
-	console.log(profile._json)
+	console.log(profile.emails)
 	// photo: photos[0].value
 	models.User.findOne({linkedinId: profile.id}).then((currentUser) => {
 		if (currentUser) {
